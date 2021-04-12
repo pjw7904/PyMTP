faf1f205a380ce5da998133aa1d0366720f0a35e
pjw7904 <pjw7904@g.rit.edu>
2021-04-12 00:37:25 -0400

    Testing out a field to get the length of a string field

--- Test/ExampleProtocolTester.py
+++ Test/ExampleProtocolTester.py
@@ -1,7 +1,8 @@
 from scapy.all import * # import all of the default scapy library
-from ExampleProtocol import Disney # import the custom Disney protocol I defined
+from ExampleProtocol import Disney, Disney2 # import the custom Disney protocol I defined
 
 def main():
+    '''
     # Create a raw packet that contains only a Disney protocol header
     testDisneyPacket = Disney(mickey=3, minnie=20, donald=3)
 
@@ -10,6 +11,14 @@ def main():
 
     # Print out the raw packet content / what is in the Disney header
     testDisneyPacket.show()
+    '''
+
+    testDisney2Packet = Disney2(Cost=69, Path="thisisaprettycooltestIhopethisworksthatwouldbesweet")
+
+    ls(testDisney2Packet)
+    testDisney2Packet.show()
+
+    testDisney2Packet.show2()
 
 if __name__ == "__main__":
     main() # run main
