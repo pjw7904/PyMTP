288f854c8b9f8f967165da35127e33ab6ace2a08
pjw7904 <pjw7904@g.rit.edu>
2021-04-28 22:11:59 -0400

    Consolidating dirs, stable Scapy-based MTP header.

---
+++ mtptest.py
@@ -0,0 +1,16 @@
+from scapy.all import * # import all of the default scapy library
+from mtp import MTP, MTP_Path # import MTP headers
+
+def main():
+    path1 = MTP_Path(cost=5, path="12345")
+    path2 = MTP_Path(cost=23, path="14654343")
+    pathList = [path1, path2]
+
+    testMTPFrame = MTP(type=3, Operation=1, Paths=pathList)
+
+    ls(testMTPFrame)
+    testMTPFrame.show()
+    testMTPFrame.show2()
+
+if __name__ == "__main__":
+    main() # run main
