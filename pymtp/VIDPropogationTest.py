d0b7f6059a5913f6a71dd99a24f0415be6066b51
pjw7904 <pjw7904@g.rit.edu>
2021-06-03 12:17:57 -0400

    Updates test MTP response and parsing logic.

--- pymtp/VIDPropogationTest.py
+++ pymtp/VIDPropogationTest.py
@@ -1,6 +1,6 @@
 '''
 Author: Peter Willis (pjw7904@rit.edu)
-Last Updated: 05/19/2021
+Last Updated: 05/20/2021
 Desc: A basic script to test a basic MT_VID propogation system. To be used on Mininet, GENI, 
       or any network of Scapy-installed devices. Currently DOES NOT work for Scapy 2.4.5, use 2.4.4.
 '''
@@ -26,13 +26,47 @@ def sendMTPFrame(MT_PDU, outInt):
     return
 
 
-def respondToMTPFrame(MT_PDU):
+def respondToMTPFrame(receivedFrame):
+    frameInfo = "TBA" # String describing the recieved frame information
+
+    if(MTP in receivedFrame): # If there is an MTP header in the frame, continue on
+        print("Received MT_PDU information:")
+
+        frameInfo = "type = unknown"
+
+        if(receivedFrame[MTP].type == 1):
+            frameInfo = "Type = 1 (Hello)"
+
+        elif(receivedFrame[MTP].type == 2):
+            frameInfo = "Type = 2 (Join)"
+
+        elif(receivedFrame[MTP].type == 3):
+            frameInfo = """
+                        Type = 3 (Path Bundle Advertisement)
+                        Operation = {0}
+                        from port: {1}
+                        number of paths included: {2}
+                        path info:
+                        """.format(receivedFrame[MTP].operation, receivedFrame[MTP].port, 
+                                   receivedFrame[MTP].count)
+            
+            pathNo = 1
+            for path in receivedFrame[MTP].paths:
+                frameInfo = frameInfo + "--Path {0}--\nCost = {1}\nLength = {2}\nPath = {3}".format(pathNo, path.cost, path.length, path.path)
+                pathNo += 1
     
-    return "test"
+    else:
+        frameInfo = "Frame recieved, does not contain an MTP header"
+
+    print(frameInfo)
+
+    return
 
 
 def main():
-    broadcastAddr = "ff:ff:ff:ff:ff:ff"
+    # Set constants for communication
+    # The multicast addr (01:80:C2:00:00:0F) isn't picked up for sniff(), so just all ffs for now
+    broadcastAddr = "01:80:c2:00:00:ff" # Reserved IEEE non-forwarded multicast address
 
     # Read in command-line arguments for start-up Meshed Tree Switch configuration, if necessary
     argParser = argparse.ArgumentParser(description = "MTP Implementation Simulator")
@@ -57,7 +91,7 @@ def main():
 
     else:
         # NOTE: Scapy ver. 2.4.5 has a bug which does not allow for multi-int sniffing, use 2.4.4
-        sniff(iface=intList, prn=lambda x: x.show())
+        sniff(iface=intList, monitor=True, prn=respondToMTPFrame)
 
 
 if __name__ == "__main__":
