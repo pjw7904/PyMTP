69cecf55d525cbd8062f98f9502e9d28acbd7366
pjw7904 <pjw7904@g.rit.edu>
2021-07-02 10:39:26 -0400

    Putting the init script back in pymtp

---
+++ pymtp/initCmds.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get -y install chrony python3-pip
+sudo sed -i '/^$/d; /#/d; s/pool/#pool/g; $ a\\n#MTP/PyMTP Research Time Server Information (questions? pjw7904@rit.edu)\nserver time.google.com iburst' /etc/chrony/chrony.conf
+sudo invoke-rc.d chrony restart && sudo chronyc -a 'burst 4/4' && sudo chronyc -a makestep
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
+sudo python3 -m pip install scapy configparser
