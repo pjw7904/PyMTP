7f1b0680e918e4951ff2bb0899b976269ee9c4c2
pjw7904 <pjw7904@g.rit.edu>
2021-07-02 10:48:11 -0400

    Updated Scapy install cmd to force ver. 2.4.4

--- setup/initCmds.sh
+++ setup/initCmds.sh
@@ -3,4 +3,4 @@ sudo apt-get -y install chrony python3-pip
 sudo sed -i '/^$/d; /#/d; s/pool/#pool/g; $ a\\n#MTP/PyMTP Research Time Server Information (questions? pjw7904@rit.edu)\nserver time.google.com iburst' /etc/chrony/chrony.conf
 sudo invoke-rc.d chrony restart && sudo chronyc -a 'burst 4/4' && sudo chronyc -a makestep
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
-sudo python3 -m pip install scapy configparser
+sudo python3 -m pip install scapy==2.4.4 configparser
