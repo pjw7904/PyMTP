84a1b599d2a8cc7a364b0c5a5388b0c593fee2f6
pjw7904 <pjw7904@g.rit.edu>
2021-08-09 22:46:51 -0400

    Added init cmd to turn off IPv4 routing on linux

--- pymtp/initCmds.sh
+++ pymtp/initCmds.sh
@@ -4,3 +4,4 @@ sudo sed -i '/^$/d; /#/d; s/pool/#pool/g; $ a\\n#MTP/PyMTP Research Time Server 
 sudo invoke-rc.d chrony restart && sudo chronyc -a 'burst 4/4' && sudo chronyc -a makestep
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
 sudo python3 -m pip install scapy==2.4.4 configparser netifaces
+sudo sysctl -w net.ipv4.ip_forward=0
