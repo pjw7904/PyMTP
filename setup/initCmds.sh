692bb93d5078030c2bf349b6dccafa366eece3e0
Peter Willis <pjw7904@g.rit.edu>
2021-09-21 12:10:09 -0400

    Updates to stop IP forwarding on nodes.

--- setup/initCmds.sh
+++ setup/initCmds.sh
@@ -3,4 +3,5 @@ sudo apt-get -y install chrony python3-pip
 sudo sed -i '/^$/d; /#/d; s/pool/#pool/g; $ a\\n#MTP/PyMTP Research Time Server Information (questions? pjw7904@rit.edu)\nserver time.google.com iburst' /etc/chrony/chrony.conf
 sudo invoke-rc.d chrony restart && sudo chronyc -a 'burst 4/4' && sudo chronyc -a makestep
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
-sudo python3 -m pip install scapy==2.4.4 configparser
+sudo python3 -m pip install scapy==2.4.4 configparser netifaces
+sudo sysctl -w net.ipv4.ip_forward=0
