f2707396415aea151d02ded17c199b7751a23da9
Peter Willis <pjw7904@rit.edu>
2021-09-26 14:50:33 -0400

    funcs for getting ipv4 addr, mask, and net ID

--- pymtp/NetUtils.py
+++ pymtp/NetUtils.py
@@ -5,7 +5,7 @@ Desc: Misc utility functions that gather information about the local box's netwo
 '''
 from socket import if_nameindex # socket stuff
 from scapy.all import get_if_hwaddr, get_if_list
-from netifaces import ifaddresses, AF_LINK
+from netifaces import ifaddresses, ip_network, AF_INET, AF_LINK
 
 # Gets the names of local interfaces (ex: eth1)
 def getLocalInterfaces():
@@ -17,10 +17,22 @@ def getLocalInterfaces():
     
     return filteredIntList
 
+# Get an IPv4 address from an interface name
+def getIPv4Address(intf):
+    return ifaddresses(intf)[AF_INET][0]['addr']
+    
+# Get an IPv4 subnet mask from an interface name
+def getIPv4NetMask(intf):
+    return ifaddresses(intf)[AF_INET][0]['netmask']
+
+# Get the Network ID for a given network
+def getIPv4NetworkID(ipv4Addr, ipv4Mask):
+    ipv4Info = "{addr}/{mask}".format(addr=ipv4Addr,mask=ipv4Mask)
+    return ip_network(ipv4Info, strict=False)
+
 # Get a MAC address from an interface name
 def getMACAddress(intf):
-    macAddress = ifaddresses(intf)[AF_LINK][0]['addr']
-    return macAddress
+    return ifaddresses(intf)[AF_LINK][0]['addr']
 
 # Crafts the appropriate Berkley Packet Filter (BPF) string for Scapy sniffing on an MTP switch (currently not used)
 def getLocalMACAddressesFilter():
