830644436f521d37a5afa74bd566e4ee7f4b1716
Peter Willis <pjw7904@rit.edu>
2021-09-29 11:56:37 -0400

    Redesigned routing for unknown dest leaf IDs

--- pymtp/PIDTable.py
+++ pymtp/PIDTable.py
@@ -7,6 +7,7 @@ Desc: Class to define the structure, logic, and behaviors of an MTP Path Identif
 from collections import namedtuple
 from datetime import datetime
 from NetUtils import getIPv4NetworkID
+import logging
 
 # Table entry format for a node which is a compute node attached to this node
 EdgeEntry = namedtuple("Edge_Entry", "IPv4Network")
@@ -15,27 +16,30 @@ EdgeEntry = namedtuple("Edge_Entry", "IPv4Network")
 DownstreamEntry = namedtuple("Downstream_Entry", "leafID spineID Cost LastTimestamp")
 
 # Table entry format for a node which is a child of this node
-UpstreamEntry = namedtuple("Upstream_Entry", "PID EgressPort LastTimestamp")
+UpstreamEntry = namedtuple("Upstream_Entry", "leafID PID EgressPort LastTimestamp")
 
 # Table entry format for an interface that hasn't been modified yet
-DefaultEntry = namedtuple("Default_Entry", "defaultValue")
+DefaultEntry = namedtuple("Default_Entry", "leafID defaultValue")
 
 class PIDTable:
     # Constructor, initalize the table itself (a dictionary)
     def __init__(self, intfList):
         self.table = {}
         self.intfDict = {}
+        self.nonComputeIntfs = []
         self.portKey = 0
 
         for intf in intfList:
-            self.table[intf] = DefaultEntry("unused")
+            self.table[intf] = DefaultEntry(-1, "unused")
+            self.nonComputeIntfs.append(intf)
 
     def addComputeSubnet(self, IPv4Address, IPv4Mask, intf):
         try:
             self.table[intf] = EdgeEntry(getIPv4NetworkID(IPv4Address, IPv4Mask))
+            self.nonComputeIntfs.remove(intf)
         except ValueError:
             print("{0} {1} is not a valid IPv4 network, rejecting entry".format(IPv4Address, IPv4Mask))
-            self.table[intf] = DefaultEntry("unused")
+            self.table[intf] = DefaultEntry(-1, "unused")
 
         return
 
@@ -52,11 +56,27 @@ class PIDTable:
 
         PID = "{LID}.{SID}".format(LID=leafID, SID=spineID)
 
-        self.table[intf] = UpstreamEntry(PID, intf, datetime.now().time())
+        self.table[intf] = UpstreamEntry(leafID, PID, intf, datetime.now().time())
         self.intfDict[self.portKey] = intf
         self.portKey += 1
         
         return
+
+    #BUG: You never know about other pod's leaf IDs, you just know about your own, so upstream ones will keep going until you hit the top, which will know if its correct or not
+    def getEgressInterface(self, srcLeafID, dstLeafID):
+        egressIntf = "None"
+
+        for intf in self.table:
+            if isinstance(self.table[intf], DownstreamEntry) and dstLeafID == self.table[intf].leafID:
+                egressIntf = intf
+                logging.debug("\n[routing] Msg routed DOWN towards destination")
+                break
+        
+        if(egressIntf == "None"):
+            egressIntf = self.getEgressUpstreamInterface(srcLeafID, dstLeafID)
+            logging.debug("\n[routing] Msg routed UP")
+
+        return egressIntf
 
     # Determines which interface/leaf node to send a packet to, utilizes MTP routed header information
     def getEgressDownstreamInterface(self, dstLeafID):
@@ -77,6 +97,12 @@ class PIDTable:
         egressIntfName = self.intfDict[egressIntfNum]
 
         return egressIntfName
+    
+    def getChildPID(self, intf):
+        return self.table[intf].PID
+
+    def getNonComputeInterfaces(self):
+        return self.nonComputeIntfs
 
     # Prints table entries
     def getTables(self):
